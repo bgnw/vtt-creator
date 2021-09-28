9c54261933b92eabf1bee9a076a5b9563e075862
Ben Agnew <48106124+bgnw@users.noreply.github.com>
2021-09-28 22:43:36 +0100

    Add layout and styling for caption component

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 $home = $_SERVER['CONTEXT_DOCUMENT_ROOT'];
 require_once "$home/incl/head.php";
 ?>
+<link rel="stylesheet" href="vtt.css" type="text/css">
+
 <body>
 
 <div class="header">
@@ -13,7 +15,13 @@ require_once "$home/incl/head.php";
 
 <div class="sep"></div>
 
-
+<div class="vtt__section">
+    <input class="vtt__time-field" id="from-min" type="text"> :
+    <input class="vtt__time-field" id="from-sec" type="text"> to
+    <input class="vtt__time-field" id="to-min" type="text"> :
+    <input class="vtt__time-field" id="to-sec" type="text">
+    <textarea class="vtt__content-field" id="content" cols="20" rows="3" ></textarea>
+</div>
 
 <?php include_once "$home/incl/footer.php"; ?>
 </body>
