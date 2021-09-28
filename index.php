cd633700bd4b09f7817e675483093153c68d93b5
Ben Agnew <48106124+bgnw@users.noreply.github.com>
2021-09-28 22:20:26 +0100

    Add inital webpage template

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$home = $_SERVER['CONTEXT_DOCUMENT_ROOT'];
+require_once "$home/incl/head.php";
+?>
+<body>
+
+<div class="header">
+    <p class="title">Ben Agnew</p>
+    <p class="subtitle">VTT Caption Creator</p>
+</div>
+
+<div class="sep"></div>
+
+
+
+<?php include_once "$home/incl/footer.php"; ?>
+</body>
+</html>
